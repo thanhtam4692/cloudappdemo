740984b70f53669eb733ff9ee35167508a4d02f4
thanhtam4692 <xitrum4692@gmail.com>
2014-02-17 12:33:13 +0700

    Add pg

--- app.js
+++ app.js
@@ -31,7 +31,9 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/users', function(req, res){
+
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
