6d0cede8dda4df27dba0bf562532d0056e49328c
thanhtam4692 <xitrum4692@gmail.com>
2014-02-18 08:26:28 +0700

    Change

--- app.js
+++ app.js
@@ -8,25 +8,15 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
-<<<<<<< HEAD
-=======
-var pg = require('pg');
-var conString = process.env.DATABASE_URL;
->>>>>>> a0719f5b2d262371e19cece96d73bf6d0157becf
 
 var app = express();
 
 // all environments
-<<<<<<< HEAD
 app.set('port', process.env.PORT || 8888);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.engine('html', require('ejs').renderFile);
-=======
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
->>>>>>> a0719f5b2d262371e19cece96d73bf6d0157becf
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -42,7 +32,6 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-<<<<<<< HEAD
 var pg = require('pg');
 
 
@@ -63,32 +52,3 @@ app.get('/', function(req, res){
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
-=======
-var client = new pg.Client(conString);
-
-
-app.get('/', routes.index);
-
-app.get('/users', function(req, res){
-	client.connect(function(err) {
-		if(err) {
-			return console.error('could not connect to postgres', err);
-		}
-		client.query('SELECT * FROM users', function(err, result) {
-			if(err) {
-				return console.error('error running query', err);
-			}
-			console.log('Result: ' + result.rows[0].username);
-			//output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
-			client.end();
-		});
-	});
-	res.send("respond with a resource" + result.rows[0].username);
-});
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
-
-
->>>>>>> a0719f5b2d262371e19cece96d73bf6d0157becf
