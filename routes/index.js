78ccf8ad7de2d556b316237d28147b9f5a2f0ad2
thanhtam4692 <xitrum4692@gmail.com>
2014-02-18 09:46:51 +0700

    Change 6

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,3 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-<<<<<<< HEAD
-  res.render('index', { title: 'Thanh Tam' });
-=======
-  res.render('index', { title: 'Express' });
->>>>>>> a0719f5b2d262371e19cece96d73bf6d0157becf
-};
